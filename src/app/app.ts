0864acf73c080d5de75684d26cc77b5570a453f9
norok <thexsonic@gmail.com>
2025-06-19 13:02:23 -0300

    feat: update app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'task-me-now';
+  protected title = 'TaskMeNow';
 }
